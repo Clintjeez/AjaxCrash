6f79ccd0bd2628850548d27c583502d221494ec9
Clinton James <clintjeez@gmail.com>
2019-10-08 01:35:28 +0100

    initial commit

---
+++ process.php
@@ -0,0 +1,27 @@
+<?php
+
+// COnnect to a database
+$conn = mysqli_connect('localhost', 'root', '', 'ajaxtest');
+
+
+echo 'Processing...';
+
+// Check for POST Variable
+if(isset($_POST['name'])){
+  $name = mysqli_real_escape_string($conn, $_POST['name']);
+  // echo 'POST: Your name is '. $_POST['name'];
+
+  $query = "INSERT INTO users(name) VALUES('$name')";
+
+  if(mysqli_query($conn, $query)){
+    echo 'User Added ..';
+  } else {
+    echo 'ERROR:'.mysqli_error($conn);
+  }
+}
+
+// Check for GET Variable
+if(isset($_GET['name'])){
+  echo 'GET: Your name is '. $_GET['name'];
+}
+
